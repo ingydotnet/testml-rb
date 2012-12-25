8303a65cc64d445f1c4a179c898ce9aa3405e294
Ingy döt Net <ingy@ingy.net>
2012-12-24 18:56:37 -0800

    1.9 style

--- test/attributes.rb
+++ test/attributes.rb
@@ -2,9 +2,9 @@ require 'testml/lite'
 require 'testml_bridge'
 
 TestML::Test.new(
-  :testname => 'test_attributes',
-  :tmlfile => 'testml/basics.tml',
-  :bridge => TestMLBridge,
-  :skip => false,
-  :plan => 123,
+  testname: 'test_attributes',
+  tmlfile: 'testml/basics.tml',
+  bridge: TestMLBridge,
+  skip: false,
+  plan: 123,
 )
