f749766aa43aeb4648486d4589c0eb9b1326ce82
Ingy döt Net <ingy@ingy.net>
2013-03-28 16:36:30 -0700

    Simplify test list

--- test/testml.rb
+++ test/testml.rb
@@ -14,20 +14,9 @@ class TestMLTestCase < Test::Unit::TestCase
     ).run(self)
   end
 
-  %w(
-    testml/arguments.tml
-    testml/assertions.tml
-    testml/basic.tml
-    testml/dataless.tml
-    testml/exceptions.tml
-    testml/external.tml
-    testml/function.tml
-    testml/label.tml
-    testml/markers.tml
-    testml/semicolons.tml
-    testml/standard.tml
-    testml/truth.tml
-    testml/types.tml
+  (Dir.glob('test/testml/*.tml')
+    .select { |f| f !~ /external\d/ }
+    .collect {|f| f.sub(/^test\//, '')}
   ).each do |file|
     method_name = 'test_' + file.gsub(/\W/, '_').sub(/_tml$/, '')
     define_method(method_name.to_sym) do
